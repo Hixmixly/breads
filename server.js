5d06b2fee163140051aa232323fbed749509c66c
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-12 20:31:59 -0700

    added model

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //dependencies
 const express = require ('express')
+const breads = require('./controllers/breads_controller.js')
 
 
 //configuration
@@ -7,10 +8,16 @@ require ('dotenv').config()
 const PORT = process.env.PORT
 const app = express ()
 
-//routes
+// ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!')
-})
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
+
 
 //listen
 app.listen(PORT, () => {
