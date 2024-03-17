4c8c7b8dad914728e06ca10fbd08b7d97307ffe7
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-16 19:32:25 -0700

    fixed show problem

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -12,12 +12,13 @@ breads.get('/', (req, res) => {
 // res.send(Bread)
 })
 
-
-
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+  res.render('Show', {
+    bread: Bread[req.params.arrayIndex]
+  })
 })
+
 
 
 module.exports = breads
