6688ed5809d78d822d8fde73c014b4406eeda64b
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-16 18:32:22 -0700

    added an array

--- views/index.jsx
+++ views/index.jsx
@@ -4,9 +4,18 @@ const Default = require('./layouts/default')
 function Index ({breads})  {
     return (
       <Default>
-        <h2>Index Page</h2>
-        <p>I have {breads[0].name} bread!</p>
-      </Default>
+      <h2>Index Page</h2>
+      {/* This is a JSX comment. */}
+      {/* <p>I have {breads[0].name} bread!</p> */} 
+
+        <ul>{
+            breads.map((bread, index) => {
+              return(<li></li>)
+            })
+
+        }</ul>
+    </Default>
+    
     )
 }
 
