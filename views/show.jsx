1bb6cefcfff013e5d0547ae36c21f47f756fb9e2
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-18 17:12:02 -0700

    css library

--- views/show.jsx
+++ views/show.jsx
@@ -2,10 +2,25 @@ const React = require('react')
 const Default = require ('./layouts/default')
 
 function Show ({bread}) {
+    //confirm were getting our bread data in the terminal
+    //console.log(bread.name)
     console.log(bread.name)
     return (
         <Default>
             <h2>Show Page</h2>
+            <h3>{bread.name}</h3>
+            <p>
+                and import 
+                {  
+                    bread.hasGluten
+                    ? <span> does</span>
+                    : <span>does not</span>
+                
+                } have gluten.
+
+            </p>
+            <img src={bread.image} alt={bread.name}/>
+            <li><a href="/breads">Go Home</a> </li>
         </Default>
     )
 }
